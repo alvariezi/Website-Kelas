d7e4dc687b773185974fe81e5640131559d397ae
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-08-04 15:15:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="w-full h-10 bg-black">
+      <div className="text-red-900 text-4xl text-center">Hello World</div>
+      </div>
+    </>
+  )
+}
+
+export default App
