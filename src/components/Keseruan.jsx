dde7cdbaa86bebeb11a1113413c6410cb9401ea2
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-09-18 11:19:54 +0700

    finsihing front end

--- src/components/Keseruan.jsx
+++ src/components/Keseruan.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Keseruan = () => {
   const [randomKekompakan, setRandomKekompakan] = useState(0);
@@ -11,11 +11,11 @@ const Keseruan = () => {
     };
 
     const kekompakanInterval = setInterval(() => {
-      setRandomKekompakan(generateRandomNumber(0, 1)); 
+      setRandomKekompakan(generateRandomNumber(0, 1));
     }, 100);
 
     const tataKramaInterval = setInterval(() => {
-      setRandomTataKrama(generateRandomNumber(0, 1)); 
+      setRandomTataKrama(generateRandomNumber(0, 1));
     }, 100);
 
     const RandomKelas = setInterval(() => {
@@ -26,48 +26,51 @@ const Keseruan = () => {
       clearInterval(kekompakanInterval);
       clearInterval(tataKramaInterval);
       clearInterval(RandomKelas);
-      setRandomKekompakan(0.01); 
-      setRandomTataKrama(0.5);   
+      setRandomKekompakan(0.01);
+      setRandomTataKrama(0.5);
       setRandomKelas(34);
     }, 5000);
 
     return () => {
       clearInterval(kekompakanInterval);
       clearInterval(tataKramaInterval);
-      clearInterval(RandomKelas)
+      clearInterval(RandomKelas);
     };
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center md:mt-10 mb-10 max-h-screen">
       <h1 className="text-3xl sm:text-4xl font-bold mb-8 ">
-        Keseruan <span className='text-[#604CC3]'>Kelas</span>
+        Keseruan <span className="text-[#604CC3]">Kelas</span>
       </h1>
 
       <div className="flex flex-col sm:flex-row justify-around w-full">
-        
         <div className="flex flex-col items-center">
           <span className="text-[55px] font-bold">
-            {randomKekompakan} <span className='text-[#604CC3]'>%</span>
+            {randomKekompakan} <span className="text-[#604CC3]">%</span>
           </span>
-          <span className="text-[31px] font-semibold text-[#9A9A9A]">Kekompakan</span>
+          <span className="text-[31px] font-semibold text-[#9A9A9A]">
+            Kekompakan
+          </span>
         </div>
 
         <div className="flex flex-col items-center">
           <span className="text-[55px] font-bold">
-            {randomTataKrama} <span className='text-[#604CC3]'>%</span>
+            {randomTataKrama} <span className="text-[#604CC3]">%</span>
           </span>
-          <span className="text-[31px] font-semibold text-[#9A9A9A]">Tata Krama</span>
+          <span className="text-[31px] font-semibold text-[#9A9A9A]">
+            Tata Krama
+          </span>
         </div>
 
         <div className="flex flex-col items-center">
-          <span className="text-[55px] font-bold">
-            {RandomKelas}
+          <span className="text-[55px] font-bold">{RandomKelas}</span>
+          <span className="text-[31px] font-semibold text-[#9A9A9A]">
+            Jumlah Siswa
           </span>
-          <span className="text-[31px] font-semibold text-[#9A9A9A]">Jumlah Siswa</span>
         </div>
-        
       </div>
+      <div className="lg:w-[1238.5px] h-[1px] bg-[#D4D4D4] mt-10"></div>
     </div>
   );
 };
--- src/pages/LandingPages.jsx
+++ src/pages/LandingPages.jsx
@@ -1,4 +1,4 @@
-import Contact from "../components/Contact";
+import MessageForm from "../components/Contact";
 import HeroSection from "../components/Hero";
 import Keseruan from "../components/Keseruan";
 import MomentSection from "../components/Moment";
@@ -6,16 +6,16 @@ import Navbar from "../components/Navbar";
 import ProfileSection from "../components/Profil";
 
 const LandingPages = () => {
-    return ( 
-        <>
-        <Navbar />
-        <HeroSection />
-        <Keseruan />
-        <MomentSection />
-        <ProfileSection  />
-        <Contact/>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Keseruan />
+      <MomentSection />
+      <ProfileSection />
+      <MessageForm />
+    </>
+  );
+};
+
 export default LandingPages;
